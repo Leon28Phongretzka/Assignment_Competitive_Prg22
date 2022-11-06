d04f2c69b044d75d0ea7a65e621efc82ed94916f
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2022-11-06 10:02:58 +0700

    05 11 22

--- customer.cpp
+++ customer.cpp
@@ -38,12 +38,13 @@ void Customer::readCustomer()
             name += tokens[i];
         }
         // use map data to store data
-        data[to_string(ID)] = name;
+        data[ID] = name;
         // set wide between name and email
-        int wide = 30 - name.length();
-        // cout << wide << endl;
-        data[to_string(ID)] += string(wide, ' ');
-        data[to_string(ID)] = data[to_string(ID)] + mail + '\t' + phone + '\t' + amount + '\t' + currency;
+        int wide = 27;
+        int wide2= 15;
+        data[ID] += string(wide - name.length(), ' ');
+        // set wide between email and phone
+        data[ID] = data[ID] + mail + string(wide - mail.length(), ' ') + phone + "\t" + amount + string(wide2 - amount.length(), ' ') + currency;
     }
 }
 void Customer::writeCustomer()
@@ -129,53 +130,43 @@ void Customer::menuCustomer()
         cin >> choice;
         switch (choice)
         {
-        case 1:
-            addCustomer();
-            break;
-        case 2:
-            updateCustomer();
-            break;
-        case 3:
-            deleteCustomer();
-            break;
-        case 4:
-            searchCustomer();
-            break;
-        case 5:
-            displayCustomer();
-            break;
-        case 6:
-            CheckValidMail(string email);
-            break;
-        case 7:
-            NoName();
-            break;
-        case 8:
-            RoundMoney();
-            break;
-        case 9:
-            TypeOfMail();
-            break;
-        case 10:
-            TypeOfCurrency(string currency);
-            break;
-        case 0:
-            cout << "Exit" << endl;
-            break;
-        default:
-            cout << "Invalid choice" << endl;
-            break;
+            case 1:
+                addCustomer();
+                break;
+            case 2:
+                updateCustomer();
+                break;
+            case 3:
+                deleteCustomer();
+                break;
+            case 4:
+                searchCustomer();
+                break;
+            case 5:
+                displayCustomer();
+                break;
+            case 6:
+                checkMail();
+                break;
+            case 7:
+                moneyToCustomer();
+                break;
+            case 8:
+                RoundMoney();
+                break;
+            case 9:
+                TypeOfMail();
+                break;
+            case 10:
+                TypeOfCurrency(string currency);
+                break;
         }
-    } while (choice != 0);
+    }
+    while (choice != 0);
 }
 void Customer::addCustomer()
 {
-    // add customer to file data1.txt
-    ofstream file;
-    file.open("D:\\4.Code\\ASG_CP22\\data1.txt", ios::app);
-    file << "ID\tName\tEmail\tPhone\tAmount\tCurrency" << endl;
-    file << ID << "\t" << name << "\t" << email << "\t" << phone << "\t" << acNo << "\t" << currency << endl;
-    file.close();
+    
 }
 void Customer::updateCustomer()
 {
@@ -187,55 +178,30 @@ void Customer::deleteCustomer()
 }
 void Customer::searchCustomer()
 {
-    
+ 
 }
 void Customer::displayCustomer()
 {
-    
+    // Get ID
 }
-void Customer::CheckValidMail(string email)
+void Customer::checkMail()
 {
-    // check valid mail use regex
-    regex pattern("(\\w+)(\\.|_)?(\\w*)@(\\w+)(\\.(\\w+))+");
-    if (regex_match(email, pattern))
-    {
-        cout << "Valid email" << endl;
-    }
-    else
-    {
-        cout << "Invalid email" << endl;
-    }
+
 }
-void Customer::NoName()
+void Customer::moneyToCustomer()
 {
-    
+
 }
 void Customer::RoundMoney()
 {
-    
+
 }
 void Customer::TypeOfMail()
 {
     
 }
-void Customer::TypeOfCurrency(string currency)
+void Customer::TypeOfCurrency()
 {
-    // check type of currency in file data1.txt
-    if (currency == "USD")
-    {
-        cout << "Dollar" << endl;
-    }
-    else if (currency == "VND")
-    {
-        cout << "Viet Nam Dong" << endl;
-    }
-    else if (currency == "EURO")
-    {
-        cout << "€" << endl;
-    }
-    else if (currency == "GBP")
-    {
-        cout << " £" << endl;
-    }
-}
+    
 
+}
--- customer.h
+++ customer.h
@@ -14,38 +14,38 @@ class Customer
         string acNo;
         string currency;
         map<int,string> tokens;
-        map<string, string> data;
+        map<int, string> data;
     public:
-        Customer();                                                                                // destructor
-        Customer(string ID, string name, string phone, string email, string acNo, string currency);// constructor
-        ~Customer();                                                                               // destructor
-        void menuCustomer();                                                                       // menu to pick customer's function
-        void readCustomer();                                                                       // Read customer data from file text
-        void writeCustomer();                                                                      // Write customer data to file text
+        Customer();                                                                                     // destructor
+        Customer(string ID, string name, string phone, string email, string acNo, string currency);     // constructor
+        ~Customer();                                                                                    // destructor
+        void menuCustomer();                                                                            // menu to pick customer's function
+        void readCustomer();                                                                            // Read customer data from file text
+        void writeCustomer();                                                                           // Write customer data to file text
         
-        void setID(string ID);                                                                     // set ID
-        void setName(string name);                                                                 // set name
-        void setPhone(string phone);                                                               // set phone
-        void setEmail(string email);                                                               // set email
-        void setAcNo(string acNo);                                                                 // set account number
-        void setCurrency(string currency);                                                         // set currency
+        void setID(string ID);                                                                          // set ID
+        void setName(string name);                                                                      // set name
+        void setPhone(string phone);                                                                    // set phone
+        void setEmail(string email);                                                                    // set email
+        void setAcNo(string acNo);                                                                      // set account number
+        void setCurrency(string currency);                                                              // set currency
 
-        string getID();                                                                            // get customer's ID
-        string getName();                                                                          // get customer's name
-        string getPhone();                                                                         // get customer's phone
-        string getEmail();                                                                         // get customer's email
-        string getAcNo();                                                                          // get customer's account number
-        string getCurrency();                                                                      // get customer's currency
+        string getID();                                                                                 // get customer's ID
+        string getName();                                                                               // get customer's name
+        string getPhone();                                                                              // get customer's phone
+        string getEmail();                                                                              // get customer's email
+        string getAcNo();                                                                               // get customer's account number
+        string getCurrency();                                                                           // get customer's currency
 
-        void addCustomer();                                                                        // Add customer data to file text
-        void updateCustomer();                                                                     // Update customer data to file text
-        void deleteCustomer();                                                                     // Delete customer data to file text
-        void searchCustomer();                                                                     // Search customer data to file text
-        void displayCustomer();                                                                    // Display customer data to file text
-        void CheckValidMail(string mail);                                                          // Money from customer
-        void NoName();                                                                             // Money to customer
-        void RoundMoney();                                                                         // Round money
-        void TypeOfMail();                                                                         // Type of mail
-        void TypeOfCurrency(string currency);                                                      // Type of Currency  
-};
+        void addCustomer();                                                                        
+        void updateCustomer();                                                                     
+        void deleteCustomer();                                                                     
+        void searchCustomer();                                                                     
+        void displayCustomer(string ID);                                                                    
+        void checkMail(string mail);                                                               
+        void moneyToCustome();                                                                             
+        void RoundMoney();                                                                         
+        void TypeOfMail();                                                                         
+        void TypeOfCurrency(string currency);    
+};                                                    
 #endif
