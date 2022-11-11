91fe0d2c0f2901fe1d3cc9f7f6924401de4a084b
Leon28Phongretzka <lamperougelelouchanm@gmail.com>
2022-11-11 22:30:56 +0700

    11 11 22

--- customer.h
+++ customer.h
@@ -21,44 +21,44 @@ class Customer
         string feedback;
         string address;
     public:
-        Customer();
-        Customer(string ID, string name, string email, string phone, string bill, string currency, string service, string feedback, string address);
-        ~Customer();
+        Customer();                                                                                                                                     // default constructor
+        Customer(string ID, string name, string email, string phone, string bill, string currency, string service, string feedback, string address);    // constructor with parameters
+        ~Customer();                                                                                                                                    // destructor
         
-        void setID(string ID);
-        void setName(string name);
-        void setPhone(string phone);
-        void setEmail(string email);
-        void setBill(string bill);
-        void setCurrency(string currency);
-        void setService(string service);
-        void setFeedback(string feedback);
-        void setAddress(string address);
+        void setID(string ID);                                                                                                                          // set ID
+        void setName(string name);                                                                                                                      // set name
+        void setPhone(string phone);                                                                                                                    // set phone
+        void setEmail(string email);                                                                                                                    // set email
+        void setBill(string bill);                                                                                                                      // set bill
+        void setCurrency(string currency);                                                                                                              // set currency
+        void setService(string service);                                                                                                                // set service
+        void setFeedback(string feedback);                                                                                                              // set feedback
+        void setAddress(string address);                                                                                                                // set address
 
-        string getID();
-        string getName();
-        string getPhone();
-        string getEmail();
-        string getBill();
-        string getCurrency();
-        string getService();
-        string getFeedback();
+        string getID();                                                                                                                                 // get ID
+        string getName();                                                                                                                               // get name
+        string getPhone();                                                                                                                              // get phone
+        string getEmail();                                                                                                                              // get email
+        string getBill();                                                                                                                               // get bill
+        string getCurrency();                                                                                                                           // get currency
+        string getService();                                                                                                                            // get service
+        string getFeedback();                                                                                                                           // get feedback
         string getAddress();
 };
 // General Functions
-void swap(Customer &a, Customer &b);    
-unsigned long long Str_to_int(string str);                                                                
-int partitionAsc(vector<Customer> &customer, int left, int right);                                        
-int partitionDesc(vector<Customer> &customer, int left, int right);
-void ValidateEmail(string email);  
+void swap(Customer &a, Customer &b);                                                                                                                    // swap 2 customers    
+unsigned long long Str_to_int(string str);                                                                                                              // convert string to int                                                                
+int partitionAsc(vector<Customer> &customer, int left, int right);                                                                                      // partition for quick sort ascending
+int partitionDesc(vector<Customer> &customer, int left, int right);                                                                                     // partition for quick sort descending 
+void ValidateEmail(string email);                                                                                                                       // validate email
 // Add, Edit, Delete Data
-void AddCustomer(vector<Customer> &customers);
-void EditCustomer(vector<Customer> &customers);
-void DeleteCustomerByID(vector<Customer> &customers);
+void AddCustomer(vector<Customer> &customers);                                                                                                          // add customer
+void EditCustomer(vector<Customer> &customers);                                                                                                         // edit customer
+void DeleteCustomerByID(vector<Customer> &customers);                                                                                                   // delete customer by ID
 // Sort
-void SortCustomer(vector<Customer> &customers, int left, int right, int opt, bool isAscending);                
+void SortCustomer(vector<Customer> &customers, int left, int right, int opt, bool isAscending);                                                         // sort customer 
 // Search  
-void SearchCustomer(vector<Customer> &customers,  string value, int opt);
+void SearchCustomer(vector<Customer> &customers,  string value, int opt);                                                                               // search customer
 // Additional
-void ExchangeCurrency();                                                                 
+void ExchangeCurrency();                                                                                                                                // exchange currency
 #endif
